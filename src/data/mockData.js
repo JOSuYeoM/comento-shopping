0e51946eb6b088d981ac38cf116c7571b608b694
suyeon <syjo1227@naver.com>
2022-07-21 00:52:01 +0900

    feat: 제품 가격 추가

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,6 +1,7 @@
 export const mockTheme1Produdcts = [
   {
     id: 1,
+    price: 21800,
     name: "비숑 블랙 머그잔",
     description:
       "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.",
@@ -12,6 +13,7 @@ export const mockTheme1Produdcts = [
 
   {
     id: 2,
+    price: 5000,
     name: "가열 보온 티코스터 온열 원터치 컵 받침대",
     description:
       "언제나 따뜻하게 최대 :2시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
@@ -22,6 +24,7 @@ export const mockTheme1Produdcts = [
   },
   {
     id: 3,
+    price: 5000,
     name: "벨루즈까사 솜사탕 파스텔 머그 4종 세트",
     description:
       "솜사탕처럼 부드러: 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그.",
@@ -35,6 +38,7 @@ export const mockTheme1Produdcts = [
 export const mockTheme2Produdcts = [
   {
     id: 4,
+    price: 6000,
     name: "쏘쿨 냉감 이불",
     description: "점점 더워지는 여름, 소쿨 냉감 이불 덮고 주무세요",
     thumbnail:
@@ -44,6 +48,7 @@ export const mockTheme2Produdcts = [
   },
   {
     id: 5,
+    price: 6000,
     name: "리모컨 에어 서큘레이터",
     description: "자연 바람 그대로를 담은, 리모컨 에어 서큘레이터",
     thumbnail:
@@ -54,6 +59,7 @@ export const mockTheme2Produdcts = [
 
   {
     id: 6,
+    price: 6000,
     name: "얼음 트레이 얼음틀 36구 4p + 아이스큐브",
     description:
       "여름에만 얼음을 이용하지 않는 것처럼, 이제는 냉동실에 한번 얼려서 사계절 내내 드세요",
